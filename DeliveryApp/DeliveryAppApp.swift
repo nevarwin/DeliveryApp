269e5bfc588065100d094812386c4a9658b70b18
nevarwin <ravencsolis@gmail.com>
2025-11-18 22:23:32 +0800

    Initial Commit

---
+++ DeliveryApp/DeliveryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeliveryAppApp.swift
+//  DeliveryApp
+//
+//  Created by raven on 11/18/25.
+//
+
+import SwiftUI
+
+@main
+struct DeliveryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DeliveryAppTests/DeliveryAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  DeliveryAppTests.swift
+//  DeliveryAppTests
+//
+//  Created by raven on 11/18/25.
+//
+
+import Testing
+@testable import DeliveryApp
+
+struct DeliveryAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
